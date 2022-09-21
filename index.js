194ddbca8a118352887dc5b7f0c234b958259e2e
彭勝緯 <rdcvgt963@MacBook-Air.hitronhub.home>
2022-09-21 13:32:11 +0800

    ver5- 修正點擊sidebar以外區域問題、關閉按鈕改為點擊圖片

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const menuBtn = document.querySelector('.phoneMenu');
 const sidebar = document.querySelector('.sidebar');
-const closeBtn = document.querySelector('.closeBtn');
+const closeImg = document.querySelector('.closeImg');
+const spacebar = document.querySelector('.spacebar')
+const sidebarOppend = document.querySelector('.container').style['right'] = "0%"
 
 if(menuBtn){
 	menuBtn.addEventListener('click', () =>{
-			sidebar.style["animation"]= "moveSidebar 0.5s both ";
-			// sidebar.style["animation-duration"]= "0.5s";
-			// sidebar.style["animation-fill-mode"]= "both";
-			// sidebar.style["animation-direction"]= "alternate";
-
+			sidebar.style["animation"]= "moveSidebar 0.5s both";
 	});
 }
 
-if(closeBtn){
-	closeBtn.addEventListener('click', () =>{
+
+
+if(sidebarOppend){
+	if(closeImg){
+	closeImg.addEventListener('click', () =>{
 			sidebar.style["animation"]= "moveSidebarReverse 0.5s both";
 	});
+}
+	if(spacebar){
+	spacebar.addEventListener('click', () =>{
+			sidebar.style["animation"]= "moveSidebarReverse 0.5s both";
+	});
+}
 }
